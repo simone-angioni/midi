a0819c7fa53e46234153fe912614d0de5c552ae2
aibba19 <a.ibba19@studenti.unica.it>
2023-03-28 15:48:51 +0200

    Update manage_processing.py

--- Midi_processing/manage_processing.py
+++ Midi_processing/manage_processing.py
@@ -33,22 +33,22 @@ if __name__ == "__main__":
             os.makedirs(dir_to_nes_db + 'csv/')
 
     # Call processing for rock db
-    # process_melody_chords(task='classification',
-    #                       chords_path=r'../dataset/rock/pickle/rock_chords',
-    #                       chords_csv=r'../dataset/rock/csv/rock_chords.csv',
-    #                       dataset_addr=r'../dataset/rock/midi',
-    #                       ints_path=r'../dataset/rock/pickle/ints_rock_dataset',
-    #                       csv_labelled=r'../dataset/rock/csv/rock_labelled.csv',
-    #                       final_csv_path=r'../dataset/rock/csv/rock_final.csv')
-    #
+    process_melody_chords(task='classification',
+                          chords_path=r'../dataset/rock/pickle/rock_chords',
+                          chords_csv=r'../dataset/rock/csv/rock_chords.csv',
+                          dataset_addr=r'../dataset/rock/midi',
+                          ints_path=r'../dataset/rock/pickle/ints_rock_dataset',
+                          csv_labelled=r'../dataset/rock/csv/rock_labelled.csv',
+                          final_csv_path=r'../dataset/rock/csv/rock_final.csv')
+    
     # # Call processing melody chords for classic db
-    # process_melody_chords(task='classification',
-    #                       chords_path=r'../dataset/classic/pickle/classic_chords',
-    #                       chords_csv=r'../dataset/classic/csv/classic_melody_chords.csv',
-    #                       dataset_addr=r'../dataset/classic/midi',
-    #                       ints_path=r'../dataset/classic/pickle/ints_classic_dataset',
-    #                       csv_labelled=r'../dataset/classic/csv/classic_labelled.csv',
-    #                       final_csv_path=r'../dataset/classic/csv/classic_final.csv')
+    process_melody_chords(task='classification',
+                          chords_path=r'../dataset/classic/pickle/classic_chords',
+                          chords_csv=r'../dataset/classic/csv/classic_melody_chords.csv',
+                          dataset_addr=r'../dataset/classic/midi',
+                          ints_path=r'../dataset/classic/pickle/ints_classic_dataset',
+                          csv_labelled=r'../dataset/classic/csv/classic_labelled.csv',
+                          final_csv_path=r'../dataset/classic/csv/classic_final.csv')
 
     # Call processing for generation nesdb
     process_melody_chords(task='classification',
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     # the final goal is create a pickle file with all ints representation of db songs with relative db label
     # having same number of files for each db
     # read the csv already created with the ints representation and use the csv as label
-    # read_db_csv_and_create_pickle(r'../dataset/nes/csv/nes_chords2.csv', r'../dataset/rock/csv/rock_labelled.csv',
-    #                               r'../dataset/classic/csv/classic_labelled.csv')
-    #
-    # print("Processing is finished")
+    read_db_csv_and_create_pickle(r'../dataset/nes/csv/nes_chords2.csv', r'../dataset/rock/csv/rock_labelled.csv',
+                                  r'../dataset/classic/csv/classic_labelled.csv')
+    
+    print("Processing is finished")
